cab26e166aeccf6a6e369f242811d41b62b96bdf
sageyosick <sageyosick@gmail.com>
2025-02-18 19:00:49 +0000

    self driving cars:)

--- Main.java
+++ Main.java
@@ -1,18 +1,27 @@
 // When you are ready to have your work checked, change the value below from N to Y
-// ReadyForSubmission=N
-// YOUR NAME:
+// ReadyForSubmission=Y
+// YOUR NAME: Sage Yosick
 // COLLABORATORS:
-// DATE:  
+// DATE:  2/18/2025
 
 public class Main
 {
-	public static void main(String[] args)
+	public static void main(String[] args) //method header
 	{
 		System.out.println("My thoughts on the article are:");
 		// Part 2: Invoking/calling method
-		
+		Main.printGuidingAnswers();
 	}
 	
 	// Part 1: Creating method definition
+	   //return type (void). // method identifier (printGuidingAnswers)
+	public static void printGuidingAnswers()
+	{ //method body
+		System.out.println("Self-driving cars should keep everyone on the road safe, whether that is the passenger or other people driving/walking. If the cars cannot do that then they shouldn't be able to be out on the roads.");
+		System.out.println("The car's algorithms should consider many things like the amount of people on the road and how to detect accidents or how to keep them from happening.");
+		System.out.println("The people who make them should be deciding the car's algorithm given that they have a driver's license and understand the laws in place.");
+
+
+	}
 	
 }
